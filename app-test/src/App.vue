e0a64904cb8720fe7d7a814fd1b4a6d5b2aecf05
gerry <gmathe08@gmail.com>
2024-10-22 15:15:11 +0200

    changed logic from app.vue to homeview

--- app-test/src/App.vue
+++ app-test/src/App.vue
@@ -1,60 +1,10 @@
 <template>
   <div class="bg-gradient-to-r from-stone-500 via-neutral-200 to-gray-500 min-h-screen">
     <Navbar />
-    <br>
-    <router-view />
-    <!-- Formulaire de création de tâche -->
-    <TaskForm @add-task="addTask" />
-
-    
-
-    <br>
-    
-    <!-- Tableau des tâches -->
-    <TaskTable :tasks="tasks" @edit-task="editTask" @remove-task="removeTask" />
-
-    <!-- Formulaire de modification de tâche -->
-    <TaskForm v-if="editingTask !== null" :task="editTaskData" @update-task="updateTask" />
+     <RouterView />
   </div>
 </template>
 
-<script>
-import Navbar from '@/components/Navbar.vue';
-import TaskForm from '@/components/TaskForm.vue';
-import TaskTable from '@/components/TaskTable.vue';
-
-export default {
-  components: {
-    Navbar,
-    TaskForm,
-    TaskTable,
-  },
-  data() {
-    return {
-      tasks: [],
-      editingTask: null, // Index de la tâche en cours de modification
-      editTaskData: {
-        name: '',
-        priority: '',
-        dueDate: ''
-      }
-    };
-  },
-  methods: {
-    addTask(newTask) {
-      this.tasks.push(newTask);
-    },
-    removeTask(index) {
-      this.tasks.splice(index, 1);
-    },
-    editTask(index) {
-      this.editingTask = index;
-      this.editTaskData = { ...this.tasks[index] };
-    },
-    updateTask(updatedTask) {
-      this.$set(this.tasks, this.editingTask, updatedTask);
-      this.editingTask = null;
-    },
-  }
-};
+<script setup>
+  import Navbar from '@/components/Navbar.vue';
 </script>
--- app-test/src/views/HomeView.vue
+++ app-test/src/views/HomeView.vue
@@ -1,63 +1,19 @@
 <template>
     <div class="p-8 bg-gray-100 min-h-screen overflow-y-auto">
-        
-    <Navbar />
-    <router-view />
-    <TaskForm @add-task="addTask" />
-    <TaskTable :tasks="tasks" @edit-task="editTask" @remove-task="removeTask" />
-    <TaskForm v-if="editingTask !== null" :task="editTaskData" @update-task="updateTask" />
+        <TaskForm @add-task="addTask" />
+        <TaskTable :tasks="tasks" @edit-task="editTask" @remove-task="removeTask" />
+        <TaskForm v-if="store.editingTask !== null" :task="store.editTaskData" @update-task="updateTask" />
     </div>
 </template>
-
-<!-- <script>
-import Navbar from '@/components/Navbar.vue';
-import TaskForm from '@/components/TaskForm.vue';
-import TaskTable from '@/components/TaskTable.vue';
-
-export default {
-    components: {
-    Navbar,
-    TaskForm,
-    TaskTable,
-    },
-    data() {
-    console.log(this.$store);
-    return {
-        tasks: this.$store.tasks,
-        editingTask: this.$store.editingTask,
-        editTaskData: this.$store.editTaskData,
-    };
-    },
-    methods: {
-    addTask(task) {
-        this.$store.addTask(task);
-    },
-    removeTask(index) {
-        this.$store.removeTask(index);
-    },
-    editTask(index) {
-        this.$store.editTask(index);
-    },
-    updateTask() {
-        this.$store.updateTask();
-    }
-    }
-};
-</script> -->
-
 <script setup>
-import Navbar from '@/components/Navbar.vue';
 import TaskForm from '@/components/TaskForm.vue';
 import TaskTable from '@/components/TaskTable.vue';
-
 import { useTaskStore } from '@/stores/taskStore';
+import { ref } from 'vue';
 
 const store = useTaskStore();
+const { tasks, addTask, removeTask, editTask, updateTask } = store;
 
-const tasks = store.tasks; 
-const editTaskData = store.editTaskData;
-const editingTask = store.editingTask;
-const addTask = store.addTask;
-const editTask = store.editTask;    
-const removeTask = store.removeTask;
+const editingTask = ref(null);
+
 </script>
